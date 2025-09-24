55a0f20edfc3c8d2ec2cf0e7d0b38c0e8eb35807
LucaMureddu <muredduluca22@gmail.com>
2025-09-24 17:02:49 +0200

    Add files via upload

---
+++ esercizi_tipidivariabili_casting.py
@@ -0,0 +1,58 @@
+#esercizio1
+nome = "Francesco"
+eta = 40
+citta = "Milano"
+print(nome, eta, citta)
+#esercizio2
+x = 5
+print(x)
+x = 10
+print(x)
+#esercizio3
+x = 10
+y = 20
+print("Somma:", x + y)
+#esercizio4
+x = 20
+y = 15
+x, y = y, x
+print(x, y)
+#esercizio5
+base = 5
+altezza = 10
+area = base * altezza
+print("Area rettangolo:", area)
+#esercizio6
+a = 10
+b = 3.5
+print(a + b)
+#esercizio7
+x, y, z = 10, 40, 30
+media = (x + y + z) / 3
+print("Media:", media)
+#esercizio8
+s1 = "Mi piace"
+s2 = " giocare a calcio"
+print(s1 + s2)
+#esercizio9
+print("Ciao " * 3)
+#esercizio10
+a = 15
+b = 30
+c = 45
+print(a < b)
+print(b > c)
+print(a < c)
+#esercizio11
+x = 10
+y = float(x)
+print(y)
+#esercizio12
+n = "4550"
+s = int(n)
+print(" il numero è:", s)
+#esercizio13
+print(bool(10))
+print(bool(0))
+print(bool(-100))
+print(bool(""))
